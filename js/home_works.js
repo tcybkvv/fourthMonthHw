08d5b5cc7ab3ad84e874a214a6cbb94264bce56f
tcybkvv <bakdoolottoychubekov@gmail.com>
2025-10-30 12:37:14 +0600

    secondChanges

--- js/home_works.js
+++ js/home_works.js
@@ -17,14 +17,51 @@ gmailButton.onclick = () => {
 
 //MOVE BLOCK
 const childBlock = document.querySelector('.child_block');
-let position = 0;
+const parentBlock = document.querySelector('.parent_block');
+let positionX = 0;
+let positionY = 0;
+
+const width = parentBlock.clientWidth - childBlock.offsetWidth;
+const height = parentBlock.clientHeight - childBlock.offsetHeight;
 
 const moveBlock = () => {
-    if (position < 448) {
-        position++;
-        childBlock.style.left = position + 'px';
-        requestAnimationFrame(moveBlock);
-    }
+    if (positionX < width && positionY === 0) positionX++;
+    else if (positionX >= width && positionY < height) positionY++;
+    else if (positionY >= height && positionX > 0) positionX--;
+    else if (positionX <= 0 && positionY > 0) positionY--;
+
+    childBlock.style.left = `${positionX}px`;
+    childBlock.style.top = `${positionY}px`;
+    requestAnimationFrame(moveBlock);
 };
 
 moveBlock();
+// STOPWATCH
+const seconds = document.querySelector('#seconds');
+const start = document.querySelector('#start');
+const stop = document.querySelector('#stop');
+const reset = document.querySelector('#reset');
+
+let interval;
+let secs = 0;
+
+start.onclick = () => {
+    if (!interval) {
+        interval = setInterval(() => {
+            secs++;
+            seconds.innerText = secs;
+        }, 1000);
+    }
+};
+
+stop.onclick = () => {
+    clearInterval(interval);
+    interval = null;
+};
+
+reset.onclick = () => {
+    clearInterval(interval);
+    interval = null;
+    secs = 0;
+    seconds.innerText = 0;
+};
