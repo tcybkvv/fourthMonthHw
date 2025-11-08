516755227aed180d38129af39c5df7510dc9ffdc
tcybkvv <bakdoolottoychubekov@gmail.com>
2025-11-08 18:52:14 +0600

    fifthChanges

--- js/lesson.js
+++ js/lesson.js
@@ -58,3 +58,42 @@ tabsParent.onclick = (e) => {
         })
     }
 }
+
+// CONVERTER
+
+const somInput = document.querySelector('#som');
+const usdInput = document.querySelector('#usd');
+const eurInput = document.querySelector('#eur');
+
+const converter = (element, targetElement, targetElement2) => {
+    element.oninput = () => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', '../data/converter.json');
+        xhr.setRequestHeader('Content-type', 'application/json');
+        xhr.send()
+
+        xhr.onload = () => {
+            const data = JSON.parse(xhr.response);
+            if (element.id === 'som') {
+                targetElement.value = (element.value / data.usd).toFixed(2);
+                targetElement2.value = (element.value / data.eur).toFixed(2);
+            }
+            if (element.id === 'usd') {
+                targetElement.value = (element.value * data.usd).toFixed(2);
+                targetElement2.value = (element.value * data.usdEur).toFixed(2);
+            }
+            if (element.id === 'eur') {
+                targetElement.value = (element.value * data.eur).toFixed(2);
+                targetElement2.value = (element.value * data.eurUsd).toFixed(2);
+            }
+            if (element.value === '') {
+                targetElement.value = '';
+                targetElement2.value = '';
+            }
+        }
+    }
+}
+
+converter(somInput, usdInput, eurInput)
+converter(usdInput, somInput, eurInput)
+converter(eurInput, somInput, usdInput)
